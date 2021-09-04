9eaa5b589c6a50a9c6081088c80762cc85b682f1
rocketny <nasascientist@163.com>
2021-09-05 02:04:44 +0800

    Create ps0.py

---
+++ MIT 600 problem set/ps0.py
@@ -0,0 +1,14 @@
+  
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  5 00:19:29 2021
+@author: S
+"""
+
+import numpy
+
+x = float(input("please enter a number: "))
+y = float(input("please enter another number: "))
+
+print("x**y =",str(x**y))
+print("log(x)=",numpy.log2(x))
